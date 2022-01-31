a63cc22a058361e2bdcd7bcacbe69cecdad0b01c
Daniel Sánchez <ingdanielsanchezve@gmail.com>
2022-01-31 13:53:32 +0100

    Layout improvement

--- components/Candidate.vue
+++ components/Candidate.vue
@@ -1,32 +1,37 @@
 <template>
-  <div class="candidate-card" :class="{'is-first-candidate': isCandidateSuitable}">
+  <div class="candidate-card" :class="{'is-candidate-suitable': isCandidateSuitable}">
     <div class="candidate-details-wrapper">
       <div class="candidate-avatar-wrapper">
         <img class="candidate-avatar" :src="source.avatar" />
       </div>
       <div class="candidate-details">
-        <div class="candidate-name">
-          <text-highlight :queries="searchTerm"> {{ source.name }} </text-highlight>
-        </div>
-        <div class="candidate-title">
-          <text-highlight :queries="searchTerm"> {{ source.title }} </text-highlight>
-        </div>
-        <div class="candidate-location">
-          <text-highlight :queries="searchTerm"> {{ source.address + ', ' + source.city }} </text-highlight>
+        <div class="candidate-detail-wrapper">
+          <div class="candidate-info-wrapper">
+            <div class="candidate-name">
+              <text-highlight :queries="searchTerm"> {{ source.name }} </text-highlight>
+            </div>
+            <div class="candidate-title">
+              <text-highlight :queries="searchTerm"> {{ source.title }} </text-highlight>
+            </div>
+            <div class="candidate-location">
+              <text-highlight :queries="searchTerm"> {{ source.address + ', ' + source.city }} </text-highlight>
+            </div>
+          </div>
+          <div class="candidate-contact-wrapper">
+            <div class="candidate-email">
+              <text-highlight :queries="searchTerm"> {{ source.email }} </text-highlight>
+            </div>
+          </div>
         </div>
 
         <div class="candidate-select">
-          <a @click="setCandidateSuitable(source)">
+          <a @click.prevent="setCandidateSuitable(source)">
             {{ isCandidateSuitable ? 'Skip selection' : 'Mark as suitable' }}
           </a>
         </div>
       </div>
     </div>
-    <div class="candidate-contact-wrapper">
-      <div class="candidate-email">
-        <text-highlight :queries="searchTerm"> {{ source.email }} </text-highlight>
-      </div>
-    </div>
+
   </div>
 </template>
 
@@ -74,7 +79,7 @@ export default {
     background: #fff73b;
   }
 
-  &.is-first-candidate {
+  &.is-candidate-suitable {
     border: 1px solid #4765FF;
     filter: drop-shadow( 0px 2px 2px rgba(0, 0, 0, 0.24));
   }
@@ -83,7 +88,6 @@ export default {
 .candidate-details-wrapper {
   display: grid;
   grid-template-columns: auto 1fr;
-  grid-gap: 28px;
 }
 
 .candidate-avatar-wrapper {
@@ -97,8 +101,36 @@ export default {
   width: 134px;
 }
 
+.candidate-detail-wrapper {
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  padding-left: 1.75em;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.12);
+
+  .is-candidate-suitable & {
+    border-bottom: none;
+  }
+}
+
+.candidate-contact-wrapper,
+.candidate-info-wrapper {
+  padding-top: 0.625em;
+}
+
 .candidate-details {
-  padding-top: 10px;
+  .candidate-select {
+    padding-left: 2em;
+    padding-top: 0.9em;
+
+    a {
+      font-size: 14px;
+      font-weight: 500;
+      line-height: 16.41px;
+      color: #009688;
+      text-transform: uppercase;
+      text-align: center;
+    }
+  }
 }
 
 .candidate-name {
@@ -120,18 +152,6 @@ export default {
   font-size: 14px;
   font-weight: 400;
   line-height: 20px;
-}
-
-.candidate-select {
-  a {
-    font-size: 14px;
-    font-weight: 500;
-    line-height: 16.41px;
-    color: #009688;
-    text-transform: uppercase;
-    margin-right: 4px;
-    padding-top: 14.5px;
-  }
 }
 
 .candidate-email {
