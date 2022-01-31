6f7db3f206ccb06a40948280880502aaa2e23298
Daniel Sánchez <ingdanielsanchezve@gmail.com>
2022-01-31 12:26:43 +0100

    Search page

---
+++ pages/search/_search.vue
@@ -0,0 +1,13 @@
+<template>
+  <CandidatesList :search-param="$route.params.search" />
+</template>
+
+<script>
+import CandidatesList from '@/components/CandidatesList'
+
+export default {
+  components: {
+     CandidatesList
+  },
+}
+</script>
